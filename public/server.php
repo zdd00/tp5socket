bbaa1deddc4cf23ed7c1cba07220892959c3b42b
zdd00 <962375670@qq.com>
2018-04-26 14:53:55 +0800

    1

--- public/server.php
+++ public/server.php
@@ -1,32 +1,37 @@
 <?php
 /**
- * workerman + GatewayWorker
- * 此文件只能在Linux运行
- * run with command
+ * run with command 
  * php start.php start
  */
+
 ini_set('display_errors', 'on');
+use Workerman\Worker;
+
 if(strpos(strtolower(PHP_OS), 'win') === 0)
 {
-	exit("start.php not support windows.\n");
+    exit("start.php not support windows, please use start_for_win.bat\n");
 }
-//检查扩展
+
+// 检查扩展
 if(!extension_loaded('pcntl'))
 {
-	exit("Please install pcntl extension. See http://doc3.workerman.net/appendices/install-extension.html\n");
+    exit("Please install pcntl extension. See http://doc3.workerman.net/appendices/install-extension.html\n");
 }
+
 if(!extension_loaded('posix'))
 {
-	exit("Please install posix extension. See http://doc3.workerman.net/appendices/install-extension.html\n");
+    exit("Please install posix extension. See http://doc3.workerman.net/appendices/install-extension.html\n");
 }
 
-define('APP_PATH', __DIR__ . '/../application/');
-define('BIND_MODULE','chat/Gate');
-// 加载框架引导文件
-require __DIR__ . '/../thinkphp/start.php';
+// 标记是全局启动
+define('GLOBAL_START', 1);
 
+require_once __DIR__ . '/vendor/autoload.php';
 
-//define('APP_PATH', __DIR__ . '/../application/');
-//define('BIND_MODULE','socket/Worker');
-//// 加载框架引导文件
-//require __DIR__ . '/../thinkphp/start.php';
+// 加载所有Applications/*/start.php，以便启动所有服务
+foreach(glob(__DIR__.'/Applications/*/start*.php') as $start_file)
+{
+    require_once $start_file;
+}
+// 运行所有服务
+Worker::runAll();
