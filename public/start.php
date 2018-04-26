7402cea45297bdb5dd8c3d72c4766f82d6420208
zdd00 <962375670@qq.com>
2018-04-26 14:44:17 +0800

    1

--- public/start.php
+++ public/start.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * run with command
+ * run with command 
  * php start.php start
  */
 
@@ -26,10 +26,10 @@ if(!extension_loaded('posix'))
 // 标记是全局启动
 define('GLOBAL_START', 1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 // 加载所有Applications/*/start.php，以便启动所有服务
-foreach(glob(__DIR__.'/../Applications/*/start*.php') as $start_file)
+foreach(glob(__DIR__.'/Applications/*/start*.php') as $start_file)
 {
     require_once $start_file;
 }
